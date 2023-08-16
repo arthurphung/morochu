4bb14336ebc973ec8db00622f362d77ee8665697
arthurphung <arthurphung.prod@gmail.com>
2023-08-16 12:54:12 -0400

    blur effect nav body

--- src/components/Header/Nav/Body/Body.tsx
+++ src/components/Header/Nav/Body/Body.tsx
@@ -2,51 +2,81 @@ import styles from './body.module.scss';
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import { translate } from '../../anim';
+import { Dispatch, SetStateAction } from 'react';
+import { blur } from '../../anim';
 
 interface Links {
-	title: string;
-	href: string;
-	src: string;
+    title: string;
+    href: string;
+    src: string;
+}
+
+interface SelectedLink {
+    isActive: boolean;
+    index: number;
 }
 
 interface BodyProps {
-	links: Links[];
+    links: Links[];
+    selectedLink: SelectedLink;
+    setSelectedLink: Dispatch<SetStateAction<SelectedLink>>;
 }
 
-export default function Body({ links }: BodyProps) {
-	const getChar = (title: string) => {
-		let chars: React.ReactElement[] = [];
-		title.split('').forEach((char, index) => {
-			chars.push(
-				<motion.span
-					key={`c_${index}`}
-					custom={[index * 0.02, (title.length - index) * 0.01]}
-					variants={translate}
-					initial='initial'
-					animate='enter'
-					exit='exit'
-				>
-					{char}
-				</motion.span>
-			);
-		});
+export default function Body({
+    links,
+    selectedLink,
+    setSelectedLink,
+}: BodyProps) {
+    const getChar = (title: string) => {
+        let chars: React.ReactElement[] = [];
+        title.split('').forEach((char, index) => {
+            chars.push(
+                <motion.span
+                    key={`c_${index}`}
+                    custom={[index * 0.02, (title.length - index) * 0.01]}
+                    variants={translate}
+                    initial='initial'
+                    animate='enter'
+                    exit='exit'
+                >
+                    {char}
+                </motion.span>
+            );
+        });
 
-		return chars;
-	};
+        return chars;
+    };
 
-	return (
-		<div className={styles.body}>
-			{links.map((link, index) => {
-				const { title, href } = link;
-				return (
-					<Link
-						key={`1_${index}`}
-						href={href}
-					>
-						<p>{getChar(title)}</p>
-					</Link>
-				);
-			})}
-		</div>
-	);
+    return (
+        <div className={styles.body}>
+            {links.map((link, index) => {
+                const { title, href } = link;
+                return (
+                    <Link
+                        key={`1_${index}`}
+                        href={href}
+                        onMouseOver={() => {
+                            setSelectedLink({ isActive: true, index });
+                        }}
+                        onMouseLeave={() => {
+                            setSelectedLink({ isActive: false, index });
+                        }}
+                    >
+                        <motion.p
+                            variants={blur}
+                            initial='initial'
+                            animate={
+                                selectedLink.isActive &&
+                                selectedLink.index != index
+                                    ? 'open'
+                                    : 'closed'
+                            }
+                        >
+                            {getChar(title)}
+                        </motion.p>
+                    </Link>
+                );
+            })}
+        </div>
+    );
 }
--- src/components/Header/Nav/Nav.tsx
+++ src/components/Header/Nav/Nav.tsx
@@ -2,56 +2,71 @@ import styles from './nav.module.scss';
 import { motion } from 'framer-motion';
 import { height } from '../anim';
 import Body from './Body/Body';
+import { useState } from 'react';
+
+interface SelectedLink {
+    isActive: boolean;
+    index: number;
+}
 
 export default function Nav() {
-	const links = [
-		{
-			title: 'Home',
-			href: '/',
-			src: 'home.png',
-		},
-		{
-			title: 'Shop',
-			href: '/shop',
-			src: 'shop.png',
-		},
-		{
-			title: 'About Us',
-			href: '/about',
-			src: 'about.png',
-		},
-		{
-			title: 'Events',
-			href: '/events',
-			src: 'events.png',
-		},
-		{
-			title: 'Merchandise',
-			href: '/merch',
-			src: 'merch.png',
-		},
-		{
-			title: 'Contact',
-			href: '/contact',
-			src: 'contact.png',
-		},
-	];
+    const links = [
+        {
+            title: 'Home',
+            href: '/',
+            src: 'home.png',
+        },
+        {
+            title: 'Shop',
+            href: '/shop',
+            src: 'shop.png',
+        },
+        {
+            title: 'About Us',
+            href: '/about',
+            src: 'about.png',
+        },
+        {
+            title: 'Events',
+            href: '/events',
+            src: 'events.png',
+        },
+        {
+            title: 'Merchandise',
+            href: '/merch',
+            src: 'merch.png',
+        },
+        {
+            title: 'Contact',
+            href: '/contact',
+            src: 'contact.png',
+        },
+    ];
 
-	return (
-		<motion.div
-			className={styles.nav}
-			variants={height}
-			initial='initial'
-			animate='enter'
-			exit='exit'
-		>
-			<div className={styles.wrapper}>
-				<div className={styles.container}>
-					<Body links={links} />
-					{/* <Footer /> */}
-				</div>
-				{/* <Image /> */}
-			</div>
-		</motion.div>
-	);
+    const [selectedLink, setSelectedLink] = useState<SelectedLink>({
+        isActive: false,
+        index: 0,
+    });
+
+    return (
+        <motion.div
+            className={styles.nav}
+            variants={height}
+            initial='initial'
+            animate='enter'
+            exit='exit'
+        >
+            <div className={styles.wrapper}>
+                <div className={styles.container}>
+                    <Body
+                        links={links}
+                        selectedLink={selectedLink}
+                        setSelectedLink={setSelectedLink}
+                    />
+                    {/* <Footer /> */}
+                </div>
+                {/* <Image /> */}
+            </div>
+        </motion.div>
+    );
 }
